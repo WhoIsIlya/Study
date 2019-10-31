b5d527feac63dd5b5bdfc7eb53953b718186a910
WhoIsIlya <worked.mail@mail.ru>
2019-10-31 18:52:44 +0300

    Update Alg1.cpp

--- src/Alg1.cpp
+++ src/Alg1.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 #include <conio.h>
 #include <fstream>
 #include <time.h>
@@ -6,59 +6,72 @@
 
 using namespace std;
 
-
-
 int main()
 {
 	vector<int> firstWordIndex;
 	vector<int> secondWordIndex;
-	ifstream file;
-	file.open("input.txt");
-	if (!file.is_open()) 
-		return 0;
-	int temp1, temp2, temp_distance;
+	int temp1, temp2, temp_distance, count = 0;;
 	int first_word_first_match = 0, first_word_last_match = 0;
 	int second_word_first_match = 0, second_word_last_match = 0;
 	int max_distance = 0, min_distance = 0;
 	string word, word1, word2;
 	word1 = "day";
 	word2 = "today";
-	int count = 0;
+	ifstream file;
+	file.open("input1.txt");
+	if (!file.is_open())
+		return 0;
+
 	clock_t start = clock();
-	
-	while (file >> word)
+
+	while (file >> word) //Чтение из файла
 	{
 		if (word == word1)
 		{
-			firstWordIndex.push_back(count);
+			firstWordIndex.push_back(count); //Индексируем совпадения первого слова и помещаем номера в вектор
 		}
 		else if (word == word2)
 		{
-			secondWordIndex.push_back(count);
+			secondWordIndex.push_back(count); //Индексируем совпадения второго слова и помещаем номера в вектор
 		}
 		count++;
 	}
 
-	for (int i = 0; i < firstWordIndex.size(); i++)
+	int minx = abs(firstWordIndex[0] - secondWordIndex[0]); //Находим начальный минимум
+	int i = 0, j = 0;
+	while (i < secondWordIndex.size() && j < secondWordIndex.size()) //Проходим оба массива один раз
 	{
-		for (int j = 0; j < secondWordIndex.size(); j++)
+		int x = firstWordIndex[i] - secondWordIndex[j]; //Разница между текущими элементами двух массивов
+		if (x < 0)										//Если она меньше нуля, то инверсируем и двигаемся дальше по первому массиву
 		{
-			temp_distance = abs(firstWordIndex[i] - secondWordIndex[j]);
-			if(temp_distance < min_distance || min_distance == 0)
-				min_distance = temp_distance - 1;
+			x = -x;
+			i++;
+		}
+		else if (x > 0)									//Если больше нуля, то смещаемся по второму массиву
+		{
+			j++;
+		}
+		else
+		{
+			minx = 0;
+			break;
+		}
+		if (x < minx)									//Если меньше текущей, то заменяем
+		{
+			minx = x;
 		}
 	}
+
+	min_distance = minx - 1;
 	temp1 = abs(firstWordIndex[0] - secondWordIndex[secondWordIndex.size() - 1])-1;
 	temp2 = abs(secondWordIndex[0] - firstWordIndex[firstWordIndex.size() - 1])-1;
-		
-
-
-	clock_t end = clock();
-	double seconds = (double)(end-start) / CLOCKS_PER_SEC;
-	if (temp1 > temp2)
+	if (temp1 > temp2)									//Находим максимальную дистанцию между первыми и последними совпадениями
 		max_distance = temp1;
 	else
 		max_distance = temp2;
+
+	clock_t end = clock();
+	double seconds = (double)(end-start) / CLOCKS_PER_SEC;
 	cout << "Max distance: " << max_distance << endl;
 	cout << "Min distance: " << min_distance << endl;
 	cout << "Words in text: " << count << endl;
